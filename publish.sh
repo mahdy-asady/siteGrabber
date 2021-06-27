6221eec5d460349df87ec38ea61645d221953b2f
Mahdy <mahdy.asady@gmail.com>
2021-06-27 18:25:22 +0430

    add release directory to .gitignore file change version format to comply with firefox rules removing js compiler

--- publish.sh
+++ publish.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #Setting variables
-VERSION=$(git describe --tags --long)
+VERSION=$(git describe --tags --long | cut -c 2- | sed 's/-/./' | sed 's/-.*//')
 
 files=("manifest.json")
+files+=("LICENSE")
 files+=("style/grabber-16.gif")
 files+=("style/grabber-32.gif")
 files+=("style/grabber.svg")
@@ -15,12 +16,12 @@ files+=("js/ext/jquery-3.5.1.min.js")
 files+=("js/ext/jszip.js")
 files+=("style/w3.css")
 
-SRCs=("js/clsProject.js")
-SRCs+=("js/db.js")
-SRCs+=("js/mainWindow.js")
-SRCs+=("js/new.js")
-SRCs+=("js/projectManager.js")
-SRCs+=("js/windowManager.js")
+files+=("js/clsProject.js")
+files+=("js/db.js")
+files+=("js/iconAnimator.js")
+files+=("js/mainWindow.js")
+files+=("js/new.js")
+files+=("js/projectManager.js")
 
 echo "**********************************************"
 echo "    Manipulating $VERSION"
@@ -35,12 +36,12 @@ done
 #set version in manifest file
 sed -i "s/%VERSION%/$VERSION/" tmp/${files[0]}
 
-echo "Compressing files..."
-for t in ${SRCs[@]}; do
-	echo "    $t ..."
-	mkdir -p $(dirname tmp/$t)
-	google-closure-compiler --js $t --js_output_file "tmp/$t"
-done
+#echo "Compressing files..."
+#for t in ${SRCs[@]}; do
+#	echo "    $t ..."
+#	mkdir -p $(dirname tmp/$t)
+#	google-closure-compiler --js $t --js_output_file "tmp/$t"
+#done
 
 echo "Adding to archive..."
 #removing previous .xpi file
